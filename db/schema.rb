3116641913536f7a69d403395fa98d19dcfa7811
Gregorio Fracassi <gregorio.fracassi@gmail.com>
2019-06-18 17:03:25 +0200

    first controller versions for user/dish/dish_availability/order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_180502) do
+ActiveRecord::Schema.define(version: 2019_06_18_142254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_06_17_180502) do
     t.bigint "dish_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
     t.index ["dish_id"], name: "index_dish_availabilities_on_dish_id"
   end
 
