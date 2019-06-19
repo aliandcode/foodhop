77f8be099c9909993754927b767758d54631307b
Aymeric Azan <aymeric.azan@gmail.com>
2019-06-19 15:51:18 +0200

    home page HTML/CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'dishes#index'
+  # root to: 'dishes#index'
+  root to: 'pages#home'
 
   resources :dishes, only: [:index, :show, :new, :create]
 
+  resources :pages do
+    collection do
+      get 'home'
+    end
+  end
 
 end
